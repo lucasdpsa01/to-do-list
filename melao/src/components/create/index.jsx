da10fefd7e379da864aff61d3d37a581dc31af0f
lucas.dpsa01 <lucasdsa2801@gmail.com>
2025-03-13 12:29:37 -0300

    pronto

--- melao/src/components/create/index.jsx
+++ melao/src/components/create/index.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react"
+
 export default function Create() {
-    return(
+    const [tarefa, setTarefa] = useState("");
+    const [lista, setLista] = useState([]);
+
+    const addTarefa = () => {
+        if (tarefa.trim() !== "") {
+            setLista([...lista, tarefa]);
+            setTarefa("");
+        }
+    };
+
+    return (
         <div className="create">
-            <textarea name="create" id="create"></textarea>
-            <button className="add">+</button>
+            <div>
+                <textarea name="create" id="create" value={tarefa} onChange={(e) => setTarefa(e.target.value)}></textarea>
+                <button className="add" onClick={addTarefa}>+</button>
+            </div>
+            <ul>
+                {lista.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
         </div>
     )
 }
--- melao/src/components/list/index.jsx
+++
@@ -1,7 +0,0 @@
-export default function list(){
-    return(
-        <div>
-            <h1>oi</h1>
-        </div>
-    )
-}
